15a0ef8eee31f1e1ba87492085d95c4d8dab5666
Francisco Geiman Thiesen <franciscogthiesen@gmail.com>
2025-12-15 14:08:26 -0800

    Fix V8 SetNativeDataProperty API compatibility
    - Cast Local<String> to Local<Name> to match v8_getter/v8_setter signatures
    - Add missing AccessControl::DEFAULT parameter for V8 API compatibility

--- javascript/mirror_bridge_v8.hpp
+++ javascript/mirror_bridge_v8.hpp
@@ -786,10 +786,16 @@ template<Bindable T>
                 static_cast<int>(name_sv.size())
             ).ToLocalChecked();
 
-            tpl->InstanceTemplate()->SetAccessor(
-                prop_name,
+            // Cast to Local<Name> to match v8_getter/v8_setter signatures
+            tpl->InstanceTemplate()->SetNativeDataProperty(
+                prop_name.As<v8::Name>(),
                 v8_getter<T, Is>,
-                v8_setter<T, Is>
+                v8_setter<T, Is>,
+                v8::Local<v8::Value>(),
+                v8::PropertyAttribute::None,
+                v8::AccessControl::DEFAULT,
+                v8::SideEffectType::kHasSideEffect,
+                v8::SideEffectType::kHasSideEffect
             );
         }(), ...);
     }(std::make_index_sequence<member_count>{});
